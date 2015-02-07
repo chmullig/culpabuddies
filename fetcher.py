bcb2967fb54fbad63e725d5a786e9728260b4adc
Chris Mulligan <chris.mulligan@gmail.com>
2015-02-07 04:35:18 -0500

    ugh

--- fetcher.py
+++ fetcher.py
@@ -10,8 +10,8 @@ mongouri = os.environ['MONGOLAB_URI']
 client = MongoClient(mongouri)
 db = client.heroku_app33788183
 
-to_fetch_depts = []
-#to_fetch_depts = [3, 4, 63, 61, 45, 41, 38, 44, 28, 56, 81, 42, 53, 29, 7, 49, 106, 52, 8, 70, 9, 58, 48, 10, 11, 12, 30, 1, 68, 57, 13, 37, 71, 15, 69, 43, 16, 17, 109, 19, 50, 20, 21, 22, 23, 92, 46, 24, 25, 62, 31, 34, 35, 66, 26, 27, 36, 40, 110, 111, 112, 114, 115]
+#to_fetch_depts = []
+to_fetch_depts = [3, 4, 63, 61, 45, 41, 38, 44, 28, 56, 81, 42, 53, 29, 7, 49, 106, 52, 8, 70, 9, 58, 48, 10, 11, 12, 30, 1, 68, 57, 13, 37, 71, 15, 69, 43, 16, 17, 109, 19, 50, 20, 21, 22, 23, 92, 46, 24, 25, 62, 31, 34, 35, 66, 26, 27, 36, 40, 110, 111, 112, 114, 115]
 
 for deptid in to_fetch_depts:
 	response = requests.get('http://api.culpa.info/departments/department_id/%s' % deptid)
@@ -40,19 +40,19 @@ for deptid in to_fetch_depts:
 		continue
 	for profinfo in responsevalue['professors']:
 		profinfo["_id"] = profinfo.pop("id")
-		profinfo["departments"] = [deptinfo]
+		profinfo["departments"] = [deptid]
 		try:
 			db.professors.insert(profinfo)
 		except pymongo.errors.DuplicateKeyError:
 			print "Already added professor %s, but adding department %s" % (profinfo, deptid)
 			prof = db.professors.find_one({"_id": profinfo["_id"]})
-			prof["departments"].append(deptinfo)
+			if deptid not in prof["departments"]:
+				prof["departments"].append(deptid)
 			db.professors.save(prof)
 
 print "now get the professors by class"
 for dept in db.departments.find():
-	toadddept = dept.copy()
-	toadddept.pop("courses")
+	deptid = dept["_id"]
 	for course in dept['courses']:
 		if 'fetched_professors' not in course:
 			response = requests.get('http://api.culpa.info/professors/course_id/%s' % course["id"])
@@ -62,43 +62,37 @@ for dept in db.departments.find():
 				continue
 			for profinfo in responsevalue['professors']:
 				profinfo["_id"] = profinfo.pop("id")
-				profinfo["departments"] = [toadddept]
+				profinfo["departments"] = [deptid]
 				try:
 					db.professors.insert(profinfo)
 					print "new professor!", profinfo
 				except pymongo.errors.DuplicateKeyError:
-					pass
-					# prof = db.professors.find_one({"_id": profinfo["_id"]})
-					# try:
-					# 	dept.pop("courses")
-					# except KeyError:
-					# 	pass
-					# if dept not in prof['departments']:
-					# 	print "Already added professor %s, but adding department %s" % (profinfo, dept)
-					# 	prof = db.professors.find_one({"_id": profinfo["_id"]})
-					# 	prof["departments"].append(dept)
-					# 	db.professors.save(prof)
+					prof = db.professors.find_one({"_id": profinfo["_id"]})
+					if deptid not in prof["departments"]:
+						print "Already added professor %s, but adding department %s" % (profinfo, deptid)
+						prof["departments"].append(deptid)
+						db.professors.save(prof)
 
 
-print "fetching reviews"
-for prof in db.professors.find({'fetched_reviews' : {'$exists': False} }):
-	response = requests.get('http://api.culpa.info/reviews/professor_id/%s' % str(prof["_id"]))
-	if response.status_code != 200:
-		print response, prof
-		continue
-	responsevalue = response.json()
-	if "reviews" not in responsevalue:
-			continue
-	for review in responsevalue['reviews']:
-		try:
-			review["_id"] = review.pop("id")
-		except:
-			continue
-		try:
-			db.reviews.insert(review)
-		except pymongo.errors.DuplicateKeyError:
-			pass
-	prof["fetched_reviews"] = True
-	db.professors.save(prof)
-	time.sleep(0.01)
+# print "fetching reviews"
+# for prof in db.professors.find({'fetched_reviews' : {'$exists': False} }):
+# 	response = requests.get('http://api.culpa.info/reviews/professor_id/%s' % str(prof["_id"]))
+# 	if response.status_code != 200:
+# 		print response, prof
+# 		continue
+# 	responsevalue = response.json()
+# 	if "reviews" not in responsevalue:
+# 			continue
+# 	for review in responsevalue['reviews']:
+# 		try:
+# 			review["_id"] = review.pop("id")
+# 		except:
+# 			continue
+# 		try:
+# 			db.reviews.insert(review)
+# 		except pymongo.errors.DuplicateKeyError:
+# 			pass
+# 	prof["fetched_reviews"] = True
+# 	db.professors.save(prof)
+# 	time.sleep(0.01)
 
--- hello.py
+++ hello.py
@@ -1,8 +1,72 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
+from pprint import pformat
+import numpy
+from pymongo import MongoClient
+import pymongo
+import cPickle
+from bson.binary import Binary
+from sklearn.metrics.pairwise import linear_kernel
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+mongouri = os.environ['MONGOLAB_URI']
+client = MongoClient(mongouri)
+db = client.heroku_app33788183
 
 app = Flask(__name__)
+app.debug = True
 
+
+if os.path.exists("reviews.pickle"):
+	print "loading reviews.pickle"
+	(reviews, reviews_tfidf, tfidf_vect) = cPickle.load(open("reviews.pickle", "r"))
+else:
+	print "loading from db"
+
+	reviews = []
+	for csprof in db.professors.find({"departments" : 7}):
+		reviews.extend([dict(x) for x in db.reviews.find({"professor_ids" : csprof["_id"]}).sort("_id", 1) ])
+	review_texts = []
+	for review in reviews:
+	    review.pop('review_tfidf')
+	    review_texts.append(review["review_text"])
+
+	print "data fetched, doing training."
+	tfidf_vect = TfidfVectorizer()
+	reviews_tfidf = tfidf_vect.fit_transform(review_texts)
+
+	cPickle.dump((reviews, reviews_tfidf, tfidf_vect), open("reviews.pickle", "w"), protocol=cPickle.HIGHEST_PROTOCOL)
+
+
+print "done."
 @app.route('/')
-def hello():
-    return 'Hello World!'
+def index():
+	return "hello"
+
+@app.route('/professor_id/<int:professor_id>')
+def professor(professor_id):
+	source_reviews_idx = [i for i, x in enumerate(reviews) if professor_id in x['professor_ids']]
+
+	pscore = {} #maps id to (n, average of similarities)
+	for i in source_reviews_idx:
+		gram = linear_kernel(reviews_tfidf[i], reviews_tfidf)
+		# for j, review in enumerate(reviews):
+		# 	for pid in review["professor_ids"]:
+		# 		try:
+		# 			z = pscore[pid]
+		# 		except KeyError:
+		# 			z = (0, 0.0)
+		# 		z = (z[0]+1, (gram.flatten()[j] - z[1])/(z[0]+1))
+		# 		pscore[pid] = z
+	top10 = sorted(pscore.items(), key=lambda x: x[1][1], reverse=True)[:10]
+
+	profs = [db.professors.find_one({"_id" : professor_id})]
+	for profid, score in top10:
+		match = db.professors.find_one({"_id" : profid})
+		profs.append((score, match))
+	return pformat(profs)
+	#return pformat([reviews[i] for i in top10])
+
+if __name__ == '__main__':
+    app.run()
+
