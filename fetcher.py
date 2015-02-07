6d4e76350b98c4422f23732ef9ec640f33c9a1b5
Chris Mulligan <chris.mulligan@gmail.com>
2015-02-07 02:02:45 -0500

    Fetch professors by class. Much more thorough.

--- fetcher.py
+++ fetcher.py
@@ -19,27 +19,65 @@ for deptid in to_fetch_depts:
 	responsevalue = response.json()
 	for deptinfo in responsevalue['departments']:
 		deptinfo["_id"] = deptinfo.pop("id")
+
+		courseresponse = requests.get('http://api.culpa.info/courses/department_id/%s' % deptid)
+		try:
+			deptinfo["courses"] = courseresponse.json()["courses"]
+		except:
+			pass
 		try:
 			db.departments.insert(deptinfo)
 		except pymongo.errors.DuplicateKeyError:
 			pass
 
-		#ok, now get the professors
-		response = requests.get('http://api.culpa.info/professors/department_id/%s' % deptid)
-		assert response.status_code == 200
-		responsevalue = response.json()
-		if "professors" not in responsevalue:
-			continue
-		for profinfo in responsevalue['professors']:
-			profinfo["_id"] = profinfo.pop("id")
-			profinfo["departments"] = [deptinfo]
-			try:
-				db.professors.insert(profinfo)
-			except pymongo.errors.DuplicateKeyError:
-				print "Already added professor %s, but adding department %s" % (profinfo, deptid)
-				prof = db.professors.find_one({"_id": profinfo["_id"]})
-				prof["departments"].append(deptinfo)
-				db.professors.save(prof)
+
+print "now get the professors by department"
+for deptid in to_fetch_depts:
+	response = requests.get('http://api.culpa.info/professors/department_id/%s' % deptid)
+	assert response.status_code == 200
+	responsevalue = response.json()
+	if "professors" not in responsevalue:
+		continue
+	for profinfo in responsevalue['professors']:
+		profinfo["_id"] = profinfo.pop("id")
+		profinfo["departments"] = [deptinfo]
+		try:
+			db.professors.insert(profinfo)
+		except pymongo.errors.DuplicateKeyError:
+			print "Already added professor %s, but adding department %s" % (profinfo, deptid)
+			prof = db.professors.find_one({"_id": profinfo["_id"]})
+			prof["departments"].append(deptinfo)
+			db.professors.save(prof)
+
+print "now get the professors by class"
+for dept in db.departments.find():
+	toadddept = dept.copy()
+	toadddept.pop("courses")
+	for course in dept['courses']:
+		if 'fetched_professors' not in course:
+			response = requests.get('http://api.culpa.info/professors/course_id/%s' % course["id"])
+			assert response.status_code == 200
+			responsevalue = response.json()
+			if "professors" not in responsevalue:
+				continue
+			for profinfo in responsevalue['professors']:
+				profinfo["_id"] = profinfo.pop("id")
+				profinfo["departments"] = [toadddept]
+				try:
+					db.professors.insert(profinfo)
+					print "new professor!", profinfo
+				except pymongo.errors.DuplicateKeyError:
+					pass
+					# prof = db.professors.find_one({"_id": profinfo["_id"]})
+					# try:
+					# 	dept.pop("courses")
+					# except KeyError:
+					# 	pass
+					# if dept not in prof['departments']:
+					# 	print "Already added professor %s, but adding department %s" % (profinfo, dept)
+					# 	prof = db.professors.find_one({"_id": profinfo["_id"]})
+					# 	prof["departments"].append(dept)
+					# 	db.professors.save(prof)
 
 
 print "fetching reviews"
@@ -62,5 +100,5 @@ for prof in db.professors.find({'fetched_reviews' : {'$exists': False} }):
 			pass
 	prof["fetched_reviews"] = True
 	db.professors.save(prof)
-	time.sleep(0.1)
+	time.sleep(0.01)
 
